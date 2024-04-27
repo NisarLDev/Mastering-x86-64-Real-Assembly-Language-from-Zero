9be34fbc970f24c78ccaa456677f13fe0037fb24
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-04-28 01:38:32 +0200

    Update hello_world_MacOS.asm

--- Hello_world/For_Mac/hello_world_MacOS.asm
+++ Hello_world/For_Mac/hello_world_MacOS.asm
@@ -21,4 +21,4 @@ start:    mov       rax, 0x02000004         ; system call for write
 message:  db        "Hello, World", 10      ; note the newline at the end
 
 ; Source https://cs.lmu.edu/~ray/notes/nasmtutorial/
-; NASM Tutorial. Loyola Marimount University. Ray Toal.
+; NASM Tutorial. Ray Toal. Computer Science Department, College of Science and Engineering of the Loyola Marymount University. Los Ángeles, EEUU.
